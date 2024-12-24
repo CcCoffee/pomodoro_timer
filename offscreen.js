212943d203000d84d6fb60278015bc9b481f4b0c
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2024-12-24 22:59:01 +0800

    delete unused files

--- offscreen.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'playSound') {
-        const audio = new Audio(message.soundUrl);
-        audio.play()
-            .then(() => sendResponse({ success: true }))
-            .catch((error) => sendResponse({ success: false, error: error.message }));
-        return true;
-    }
-}); 
